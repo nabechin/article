99d402369b61eba0b1651af06bc7952785519a42
nabechin <nabenabe0258@gmail.com>
2020-07-17 21:33:50 +0900

    test:APIのテスト作成

--- app/api/tests.py
+++ app/api/tests.py
@@ -5,6 +5,8 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 from account.models import UserProfile, Relation
+from article.models import Article, Comment, FavoriteArticle, FavoriteComment
+from message.models import TalkRoom, Message
 from rest_framework.test import APIClient
 from rest_framework import status
 from .serializers import UserProfileSerializer
@@ -19,15 +21,29 @@ def create_image_to_user_profile():
     return tmp_file
 
 
-# defaultでユーザ情報をdbに登録
-def create_user(user_info):
-    return get_user_model().objects.create(**user_info)
+# defaultのユーザ情報をdbに登録
+def create_user(user):
+    return get_user_model().objects.create(**user)
 
-# defaultでユーザのプロフィール情報をdbに登録
+# defaultのユーザのプロフィール情報をdbに登録
 
 
 def create_user_profile(user):
-    return UserProfile.objects.create(user=user, introduction='自己紹介テスト')
+    return UserProfile.objects.create(user=user,
+                                      introduction='自己紹介テスト')
+
+
+# defaultの記事をdbに登録
+def create_article(user):
+    return Article.objects.create(content='記事コンテンツテスト', user=user)
+
+
+def create_comment(user):
+    return Comment.objects.create(user=user, article=create_article(user), content='コメントコンテンツテスト')
+
+
+def create_talk_room(last_message):
+    return TalkRoom.objects.create(last_message=last_message)
 
 
 """ 未ログイン時にユーザのプロフィールAPIにアクセスをするテスト """
@@ -40,7 +56,7 @@ class PublicUserProfileApiTest(TestCase):
 
     # 権限のないユーザはアクセスできないことをテスト
     def test_user_profile_api_need_authenticate(self):
-        response = self.client.get('/api/profileEdit/')
+        response = self.client.get('/api/user_profile/')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -66,7 +82,7 @@ class PrivateUserProfileApiTest(TestCase):
     # 登録したユーザの自己紹介と名前をhttpのgetメソッドで呼びだし、ステータスが200を返す
     def test_retrieve_user_profile(self):
         response = self.client.get(
-            '/api/profileEdit/')
+            '/api/user_profile/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     # 登録したユーザの自己紹介と名前をhttpのpatchメソッドで更新し、ステータスが200を返す
@@ -77,7 +93,7 @@ class PrivateUserProfileApiTest(TestCase):
             'introduction': '更新後自己紹介'
         }
         response = self.client.patch(
-            '/api/profileEdit/'+str(self.user_profile.id)+'/', user_profile_after_update)
+            '/api/user_profile/'+str(self.user_profile.id)+'/', user_profile_after_update)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(
             user_profile_after_update['introduction'], response.data.get(
@@ -96,7 +112,7 @@ class PrivateUserProfileApiTest(TestCase):
             'introduction': '更新前自己紹介'
         }
         response = self.client.patch(
-            '/api/profileEdit/'+str(self.user_profile.id)+'/', user_profile_after_update, format='multipart')
+            '/api/user_profile/'+str(self.user_profile.id)+'/', user_profile_after_update, format='multipart')
         self.user_profile.refresh_from_db()
         self.user_profile = UserProfile.objects.get(pk=response.data.get('id'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -149,7 +165,199 @@ class PrivateRelationApiTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
+    # viewから登録したRelation情報が作成されていることをテスト
     def test_post_relation(self):
+        relation_info = {"follwer": self.user1.id, "target": self.user2.id}
         response = self.client.post(
-            '/api/relation/', {"target": self.user2.id})
+            '/api/relation/', relation_info)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        is_exists = Relation.objects.filter(
+            follower=self.user1.id, target=self.user2.id).exists()
+        self.assertTrue(is_exists)
+
+    # 指定のRelationレコードをhttpのdeleteメソッドで削除されていることをテスト
+    def test_delete_relation(self):
+        relation = Relation.objects.create(
+            follower=self.user1, target=self.user2)
+        relation_info = {"follower": self.user1.id, "target": self.user2.id}
+        response = self.client.delete(
+            '/api/relation/'+str(relation.id)+'/')
+        is_exists = Relation.objects.filter(
+            follower=relation_info["follower"], target=relation_info["target"]).exists()
+        self.assertFalse(is_exists)
+
+
+""" 未ログイン時にCommentAPIにアクセスするテスト """
+
+
+class PublicCommentApiTest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+
+    # 権限のないユーザはアクセスできないことをテスト
+    def test_comment_api_need_authenticate(self):
+        response = self.client.get('/api/comment/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateCommentApiTest(TestCase):
+    def setUp(self):
+        user_info = {'name': 'テスト１', 'email': 'test1@gmail.com',
+                     'username': 'test1', 'password': 'testtest'
+                     }
+        self.client = APIClient()
+        self.user = create_user(user_info)
+        self.article = create_article(self.user)
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_comment(self):
+        comment = Comment.objects.create(
+            article=self.article, user=self.user, content='コメントテスト')
+        response = self.client.get('/api/comment/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_post_comment(self):
+        comment_info = {"article": self.article.id,
+                        "user": self.user.id, "content": "テスト用コメント"}
+        response = self.client.post('/api/comment/', comment_info)
+        is_exists = Comment.objects.filter(
+            article=comment_info['article'], user=comment_info['user']).exists()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(is_exists)
+
+
+class PublicFavoriteArticleApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_favorite_article_api_need_authenticate(self):
+        response = self.client.get('/api/favorite_article/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateFavoriteArticleApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user_info = {'name': 'テスト１', 'email': 'test1@gmail.com',
+                     'username': 'test1', 'password': 'testtest'
+                     }
+        self.user = create_user(user_info)
+        self.article = create_article(self.user)
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_favorite_article(self):
+        favorite_article = FavoriteArticle.objects.create(
+            article=self.article, user=self.user
+        )
+        response = self.client.get('/api/favorite_article/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_post_favorite_article(self):
+        favorite_article_info = {"article": self.article.id,
+                                 "user": self.user.id
+                                 }
+        response = self.client.post(
+            '/api/favorite_article/', favorite_article_info)
+        is_exists = FavoriteArticle.objects.filter(
+            article=favorite_article_info["article"], user=favorite_article_info["user"]
+        ).exists()
+        self.assertTrue(is_exists)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_delete_favorite_article(self):
+        favorite_article = FavoriteArticle.objects.create(
+            article=self.article, user=self.user
+        )
+        response = self.client.delete(
+            '/api/favorite_article/'+str(favorite_article.id)+'/')
+        is_exists = FavoriteArticle.objects.filter(
+            pk=favorite_article.id).exists()
+        self.assertFalse(is_exists)
+
+
+class PublicFavoriteCommentApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_favorite_comment_api_need_authenticate(self):
+        response = self.client.get('/api/favorite_comment/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateFavoriteCommentApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user_info = {'name': 'テスト１', 'email': 'test1@gmail.com',
+                     'username': 'test1', 'password': 'testtest'
+                     }
+        self.user = create_user(user_info)
+        self.comment = create_comment(self.user)
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_favorite_comment(self):
+        FavoriteComment.objects.create(user=self.user, comment=self.comment)
+        response = self.client.get('/api/favorite_comment/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_post_favorite_comment(self):
+        favorite_comment_info = {"user": self.user.id, "comment": self.comment.id
+                                 }
+        response = self.client.post(
+            '/api/favorite_comment/', favorite_comment_info)
+        is_exists = FavoriteComment.objects.filter(
+            user=favorite_comment_info["user"], comment=favorite_comment_info["comment"]
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(is_exists)
+
+    def test_delete_favorite_comment(self):
+        favorite_comment = FavoriteComment.objects.create(
+            user=self.user, comment=self.comment)
+        response = self.client.delete(
+            '/api/favorite_comment/'+str(favorite_comment.id)+'/')
+        is_exists = FavoriteComment.objects.filter(pk=favorite_comment.id
+                                                   ).exists()
+        self.assertFalse(is_exists)
+
+
+class PublicMessageApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_message_api_need_authenticate(self):
+        response = self.client.get('/api/message/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateMessageApiTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        user_info = {'name': 'テスト１', 'email': 'test1@gmail.com',
+                     'username': 'test1', 'password': 'testtest'
+                     }
+        self.user = create_user(user_info)
+        self.talk_room = create_talk_room('テストラストメッセージ')
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_message(self):
+        Message.objects.create(talk_room=self.talk_room,
+                               sender=self.user, body='テストボディー')
+        response = self.client.get('/api/message/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+
+    def test_post_message(self):
+        message_info = {"talk_room":self.talk_room.id,"sender":self.user.id,"body":"テストメッセージ"}
+        response = self.client.post('/api/message/',message_info)
+        is_exists = Message.objects.filter(talk_room=message_info["talk_room"])
+        self.assertTrue(is_exists)
+        self.assertEqual(response.status_code,status.HTTP_201_CREATED)
+
+
+
+
